ab478ce9d3355b3387ba4f4bfe61ac02af31be8d
Jonathan Lafrenière <46546590+jolafreniere@users.noreply.github.com>
2022-08-19 01:05:32 -0400

    - Formatting - Exit the process once client has left - Change timeout (different from port number, for clarity's sake)

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,20 @@ const wss = new websocket.Server({ port: 3000 })
 
 wss.on('connection', async function connection(ws) {
     console.log("connection ...");
+
     ws.on('message', function incoming(message) {
         console.log('received: %s', message);
     });
 
+    ws.on("close", function () {
+        console.log("Client has closed the connection");
+        process.exit(0); // Exits as soon as a single client quits
+    })
+
     ws.send("SERVER: Connection established.");
+
     while(true){
-        await new Promise(resolve => setTimeout(resolve, 3000));
+        await new Promise(resolve => setTimeout(resolve, 2000));
         let msg = `SERVER SENT: ${JSON.stringify(new Date())}`;
         console.log("sending: %s", msg);
         ws.send(msg);
