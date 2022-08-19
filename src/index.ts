aee3d963e723157e0412cab872e30e22c4d4023d
Jonathan Lafrenière <46546590+jolafreniere@users.noreply.github.com>
2022-08-19 02:32:55 -0400

    - Clearly track multiple connections - Send messages at random time interval - Limit amount of messages sent to MESSAGES_PER_CONNECTION const - Close connection once message count has been reached

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,38 @@
 const websocket = require("ws");
 const wss = new websocket.Server({ port: 3000 })
+const MESSAGES_PER_CONNECTION = 5;
+let connectionCount = 0;
 
 wss.on('connection', async function connection(ws) {
-    console.log("connection ...");
+
+    let connectionId = connectionCount++;
+    let messagesSent = 0;
+    console.log(`Connection #${connectionId} established.`);
 
     ws.on('message', function incoming(message) {
         console.log('received: %s', message);
+        if(message.toString().indexOf("exit") > -1){
+            console.log("Received exit command, closing server...");
+            process.exit(0);
+        }
     });
 
     ws.on("close", function () {
-        console.log("Client has closed the connection");
-        process.exit(0); // Exits as soon as a single client quits
+        console.log(`Connection #${connectionId} has been closed.`);
     })
 
-    ws.send("SERVER: Connection established.");
+    ws.send(`[SERVER] Connection #${connectionId} established.`);
 
-    while(true){
-        await new Promise(resolve => setTimeout(resolve, 2000));
-        let msg = `SERVER SENT: ${JSON.stringify(new Date())}`;
+    while(messagesSent++ < MESSAGES_PER_CONNECTION){
+        await new Promise(resolve => setTimeout(resolve, 2987 + Math.random() * 1000));
+        let msg = `[SERVER] Sending to connection #${connectionId}: Server Message #${messagesSent}`;
         console.log("sending: %s", msg);
         ws.send(msg);
+
+        if(messagesSent == MESSAGES_PER_CONNECTION){
+            ws.close();
+            return;
+        }
     }
+
 });
